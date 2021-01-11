98f8a66b552ab0e0c964837f3882b03d23d579a1
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-01-11 12:49:35 +0000

    Adds the transformers.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,17 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        quality: 70
+      }
+    },
+    {
       resolve: "gatsby-source-wordpress-experimental",
       options: {
         url: "http://testsite.local/graphql",
       },
     },
+    `gatsby-transformer-sharp`,
   ],
 };
